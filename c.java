e446ea57e7de17315362b0a6d25e81101889681c
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-04 12:28:39 +0530

    Create c.java

---
+++ c.java
@@ -0,0 +1,21 @@
+
+public class Multiples{
+
+
+	public static void main(String[] args){
+
+
+		
+			
+				for (int i = 1; i <= 50; i++)
+				{
+				   
+					if (i % 3 == 0)
+						System.out.print(i);
+					if(i % 15== 1)System.out.println();
+			 
+					  
+				}
+			}
+
+		}
