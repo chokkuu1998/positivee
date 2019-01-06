c9cfd302d638a53a3992b247d6f1aadfd9260efa
chokkuu1998 <chokkuu1998@gmail.com>
2019-01-06 08:34:30 +0530

    Create stream.c

---
+++ stream.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+int min,mid,max,i=1;
+printf("Enter number: ");
+scanf("%d",&a);
+printf("\nEnter number: ");
+    scanf("%d",&b);
+    printf("\nEnter number: ");
+    scanf("%d",&c);
+
+    if(((a<b)&&(b<c)) || ((a>b)&&(a<c)))
+    {
+        min=a;
+        mid=b;
+        max=c;
+    }
+    if(((b<a)&&(a>c)) || ((a>b)&&(a<c)))
+    {
+        min=c;
+        mid=a;
+        max=b;
+    }
+    if(((c<a)&&(c>b)) || ((c>a)&&(c<b)))
+    {
+        min=b;
+        mid=c;
+        max=a;
+    }
+    printf("\nMid is %d",mid);
+
+
+
+    for(i=min;i<=max;i++)
+    {
+
+
+        if(i==mid)
+            continue;
+        printf("\n%d",i);
+    }
+
+    getchar();
+    getchar();
+
+}
