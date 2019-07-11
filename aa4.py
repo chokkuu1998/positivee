da6b9323a69ae129cdebdbb7093096caad59f197
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-11 14:47:31 +0530

    Update aa4.py

--- aa4.py
+++ aa4.py
@@ -1,16 +1,16 @@
 tthi = int(input())
 aaa1 = list(map(int,input().split()))
 sss,l = 0,[]
-bb1 = [x for x in range(1,tthi+1)]
+bb1 = [x for x in range(1,thi+1)]
 for i in aaa1:
   if i in bb1:
     bb1.remove(i)
 kh = 0
 for i in range(0,tthi-1):
   p = aaa1[i]
-  for j in range(i+1,thi):
+  for j in range(i+1,tthi):
     if p == aaa1[j]:
-      if p < b1[kh]:
+      if p < bb1[kh]:
         aaa1[j] = bb1[kh]
         sss += 1
       else:
